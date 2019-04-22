6b5f6945cceb9742a0e7463c480f3b0a823525cf
thollister74 <49489585+thollister74@users.noreply.github.com>
2019-04-22 15:56:34 -0400

    Java training week 2 -- Removed unnecessary variable names from parameters

--- test/Main.java
+++ test/Main.java
@@ -1,18 +1,17 @@
-package test;//
-//Created by hollisti on 2019-04-22.
-//
+package test;
 
 import java.awt.*;
+//Created by hollisti on 2019-04-22.
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        System.out.println("Total is " + 3 + 4);
-
-        Car myCar = new Car(inputAverageMPG: 25.2,
-                inputLicensePlate: "CBA-321",
+        System.out.println("Total is " + (3 + 4));
+        Car myCar = new Car(25.2,
+                "CBA-321",
                 Color.RED,
-                inputAreTailLightsWorking: true);
-
+                false);
+        System.out.println(myCar.licensePlate);
 
     }
 }
